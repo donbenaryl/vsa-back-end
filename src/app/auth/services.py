7f5f0139c0d7ab8d36354e1fd8678b78e34bd681
Don <donbenaryl@gmail.com>
2023-09-06 11:57:11 +0800

    Debug login not working

--- src/app/auth/services.py
+++ src/app/auth/services.py
@@ -22,7 +22,7 @@ def process_login(data: IUserBasicDetails):
             "msg": "Invalid username or password"
         }), 404) 
 
-    logger.info(f"Checking if password {cryptocode.decrypt('fXOufPY3a3g=*mHKjJUF8iYPrghXpMm6DcA==*G59+aEdjw8ARfDOAwbkSNQ==*e1OA8HgsTzQ6pP0bukkoRQ==', '	0x66dFA4b56678B6EdE0ab2765804EeB009dc0EE47')} correct...")
+    logger.info(f"Checking if password {cryptocode.decrypt('fXOufPY3a3g=*mHKjJUF8iYPrghXpMm6DcA==*G59+aEdjw8ARfDOAwbkSNQ==*e1OA8HgsTzQ6pP0bukkoRQ==', '0x66dFA4b56678B6EdE0ab2765804EeB009dc0EE47')} correct...")
     logger.info(f"UK: {USER.USER_KEY}")
     logger.info(f"PS: {user.password}")
     logger.info(f"Type: {type(user.password)}")

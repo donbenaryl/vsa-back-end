290f9e1ab3b885276c0e7f58196654a5176aa56d
Don <donbenaryl@gmail.com>
2023-09-07 15:13:21 +0800

    db con

--- src/config/config.py
+++ src/config/config.py
@@ -9,9 +9,9 @@ class Config(BaseModel):
     # e.g: '["http://localhost", "http://localhost:4200", "http://localhost:3000", \
     # "http://localhost:8080", "http://local.dockertoolbox.tiangolo.com"]'
     BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = ["http://localhost:4200", "http://localhost", "https://terminal.syslit.io","http://localhost:8001"]
-    DB_NAME: str = "vsa2"
-    DB_USERNAME: str = "don_user"
-    DB_PASSWORD: str = "password"
+    DB_NAME: str = "virtual_staff_avenue"
+    DB_USERNAME: str = "don159"
+    DB_PASSWORD: str = "NIdb7ft&[mco"
     DB_HOST: str = "localhost"
     DB_PORT: int = 3306
     USER_KEY: str = "0x66dFA4b56678B6"
